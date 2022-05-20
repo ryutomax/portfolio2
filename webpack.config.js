79cd1cc6c79cec198ed6ebe8ddbb450731e7a114
Ryuto Sato <r4to2web@gmail.com>
2022-05-20 23:23:55 +0900

    imgmin 追加

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,40 @@ const path = require('path')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const globImporter = require('node-sass-glob-importer')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const ImageminMozjpeg = require("imagemin-mozjpeg");
 
 module.exports = (env, argv) => {
   const PRODUCTION = argv.mode === 'production'
 
   return {
-    entry: './src/js/index.js',
+    entry: './src/js/entry.js',
 
     output: {
       filename: PRODUCTION
-        ? 'assets/javascripts/bundle-[hash].js'
-        : 'assets/javascripts/bundle.js',
+        ? 'assets/js/bundle-[hash].js'
+        : 'assets/js/bundle.js',
       path: path.join(__dirname, 'dist'),
     },
 
     plugins: [
       new CleanWebpackPlugin([
-        'dist/assets/stylesheets',
-        'dist/assets/javascripts',
+        'dist/assets/style',
+        'dist/assets/js',
       ]),
 
       new MiniCssExtractPlugin({
         filename: PRODUCTION
-          ? 'assets/stylesheets/bundle-[hash].css'
-          : 'assets/stylesheets/bundle.css',
+          ? 'assets/style/bundle-[hash].css'
+          : 'assets/style/bundle.css',
       }),
 
       // ejs
       new HtmlWebpackPlugin({
+        // inject: head,
         filename: 'index.html',
         template: 'src/ejs/index.ejs',
       }),
@@ -41,19 +44,41 @@ module.exports = (env, argv) => {
         filename: 'about/index.html',
         template: 'src/ejs/about/index.ejs',
       }),
-
-      // php
-      new CopyWebpackPlugin(
+      new CopyPlugin(
         PRODUCTION
           ? [
+              // php
               {
                 from: './src/api/*.php',
                 to: path.resolve(__dirname, 'dist/api'),
                 flatten: true,
               },
+              //圧縮した画像をsrcのimagesフォルダからコピーして、distのimagesフォルダに出力する
+              {
+                from: `${path.resolve(__dirname, "src")}/images/`,
+                to: `${path.resolve(__dirname, "dist/assets/")}/images/[name]-min.[ext]`
+              },
             ]
           : []
       ),
+      new ImageminPlugin({ //画像圧縮処理の指定
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        plugins: [
+          ImageminMozjpeg({
+            quality: 75,
+            progressive: true
+          })
+        ],
+        pngquant: {
+          quality: "70"
+        },
+        gifsicle: {
+          interlaced: false,
+          optimizationLevel: 10,
+          colors: 256
+        },
+        svgo: {}
+      })
     ],
     resolve: {
       extensions: [
@@ -134,6 +159,7 @@ module.exports = (env, argv) => {
         },
       ],
     },
+
     devServer: {
       contentBase: path.resolve(__dirname, 'dist'),
       port: 8080,
