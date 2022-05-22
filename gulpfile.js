1cc0191ff9269337b90998be7521bc76f17b415c
Ryuto Sato <r4to2web@gmail.com>
2022-05-22 23:54:49 +0900

    css-declaration-sorter　実装

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,9 @@ const plumber = require("gulp-plumber");
 const sassGlob = require("gulp-sass-glob-use-forward");
 const sass = require('gulp-sass')(require('sass'));
 // const autoprefixer = require("gulp-autoprefixer");
-// const cleancss = require("gulp-clean-css");
-// const media = require("gulp-group-css-media-queries");
-const gulpPostcss = require('gulp-postcss');
+const postcss = require('gulp-postcss');
 const cssDeclarationSorter = require('css-declaration-sorter');
 
-//----------------------------------------------------------------------
-//
-//----------------------------------------------------------------------
 function compile(done) {
   src("./src/scss/**/*.scss")
 
@@ -26,41 +21,19 @@ function compile(done) {
   .pipe( sass({
       includePaths: ['./scss/']       // sassコンパイル
   }))
-  // .pipe( autoprefixer( /           / ベンダープレフィック自動付与(sass後)
-  //   {
-  //   cascade: false
-  // }))
-  .pipe(gulpPostcss(
-      [cssDeclarationSorter({
-              order: 'smacss'
-          })]
-  ))                                 //css整列
-  // .pipe(media())                     // メディアクエリ統合
+  .pipe(postcss([cssDeclarationSorter({order: 'smacss'})]))
   .pipe(dest("./src/css/"));
 
   done();
 }
-
-// function min(done) {
-//   src("./css_origin/*.css")
-
-//   .pipe(plumber())                   // watch中にエラーが発生してもwatchが止まらないようにする
-//   .pipe(cleancss())                  // 圧縮 コードの不要なインデントや改行を削除
-//   .pipe(dest("./css_min/"));
-
-//   done();
-// }
-
 function sassWatch() {
-  // watch( "監視したいファイル(またはフォルダ)を指定" , 処理 );
   watch("./src/scss/**/*.scss" , series(compile));
 }
 exports.watch = series(sassWatch);
 
-
 // ========================================
 // img最適化
-// ========================================-----------------------------------------------------------
+// ========================================
 
 const imageMin = require("gulp-imagemin");              // npm i -D gulp-imagemin@7.1.0
 const mozjpeg = require("imagemin-mozjpeg");
@@ -68,23 +41,26 @@ const pngquant = require("imagemin-pngquant");
 const changed = require("gulp-changed");
 
 function imagemin(done) {
-    src("./src/images/*")
-    .pipe(changed("./dist/images/[name]-min.[ext]"))
-    .pipe(
-        imageMin([
-            pngquant({
-                quality: [0.6, 0.7],
-                speed: 1,
-            }),
-            mozjpeg({ quality: 65 }),
-            imageMin.svgo(),
-            imageMin.optipng(),
-            imageMin.gifsicle({ optimizationLevel: 3 }),
-        ])
-    )
-    .pipe(dest("./dist/images/"));
-
-    done();
+  src("./src/images/*")
+  .pipe(changed("./dist/assets/images/[name]-min"))
+  .pipe(
+    imageMin([
+      pngquant({
+          quality: [0.6, 0.7],
+          speed: 1,
+      }),
+      mozjpeg({ quality: 65 }),
+      imageMin.svgo(),
+      imageMin.optipng(),
+      imageMin.gifsicle({ optimizationLevel: 3 }),
+    ])
+  )
+  .pipe(dest("./dist/assets/images/"));
+  done();
 }
+function imgWatch() {
+  watch("./src/images/*" , series(imagemin));
+}
+exports.imgwatch = series(imgWatch);
 
-exports.imgMin = imagemin;
+// exports.imgMin = imagemin;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 const path = require('path');
-// const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const globImporter = require('node-sass-glob-importer');
-// const Fiber = require('fibers')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 const ImageminPlugin = require("imagemin-webpack-plugin").default;
